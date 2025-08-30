3c57dfcf462c3db1ad6e48d407c96d4d9feb2a4d
Manish Kumar <ms5798034@gmail.com>
2025-08-30 09:35:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://otp-based-notetakingapp-frontend.onrender.com',
     credentials:true
 
 }))
